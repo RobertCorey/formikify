3ca13a01ff0e37d58a1a34fe3531875e66b46c7e
Rob Corey <rob@insurify.com>
2021-03-21 11:02:39 -0400

    Simple React Bootstrap + Formik

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Formik, useField } from "formik";
+import { Button, Form } from "react-bootstrap";
+
+const FKText = () => {
+  const [field] = useField({ name: "email" });
+  return <Form.Control {...field}></Form.Control>;
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Formik
+        initialValues={{ email: "rob@poo.com" }}
+        onSubmit={(values, { setSubmitting }) => {
+          console.log(JSON.stringify(values, null, 2));
+          setSubmitting(false);
+        }}
+      >
+        {({ isSubmitting, handleReset, handleSubmit }) => (
+          <Form onReset={handleReset} onSubmit={handleSubmit}>
+            <Form.Group>
+              <FKText></FKText>
+            </Form.Group>
+            <Button type="submit">Fukc</Button>
+          </Form>
+        )}
+      </Formik>
     </div>
   );
 }
